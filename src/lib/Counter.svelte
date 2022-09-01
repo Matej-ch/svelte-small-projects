3c908e666a3799df40a82e590b0ef7ea4de0b53e
Matej <matejchalachan@gmail.com>
2022-09-01 20:06:39 +0200

    first commit

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let title='Cool title';
+
+  let count = 0
+  const increment = () => {
+    count += 1.3
+  }
+
+  const decrement = () => {
+    count -= 1.1
+  }
+</script>
+
+<button on:click={increment}>
+  count with name {title} is {count.toFixed(2)}
+</button>
+
+<button on:click={decrement}>
+  count with name {title} is {count.toFixed(2)}
+</button>
